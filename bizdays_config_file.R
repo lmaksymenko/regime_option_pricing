1c7fb2f27260cc39d41fd337f3d44f16bdd6cccb
lmaksymenko <leo.maksymenko@gmail.com>
2022-02-18 12:38:51 -0500

    trading calender 2??

--- bizdays_config_file.R
+++ bizdays_config_file.R
@@ -2,11 +2,13 @@
 
 library(bizdays)
 library(libridate)
+
 rm(list = ls())
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-df = read.csv(paste(getwd(), "/utils/", "market_days_1980_2030.csv", sep = ''), header = FALSE)
-
+df = read.csv(paste(getwd(), 
+                    "/utils/", "market_days_1980_2030.csv", sep = ''), 
+                    header = FALSE)
 hol = c(paste(df[,'V3'], '-', df[,'V2'], '-', df[,'V4'], sep = ''))
 
 create.calendar(name = 'USA 1980-2030',
